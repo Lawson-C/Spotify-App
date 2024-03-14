2e0076a86f19ff938db633aaf4c5357d3353a795
aadarsh-v <aadarsh.vavilikolanu@gmail.com>
2024-03-14 03:00:24 -0400

    Update the login screen to work with the database.

--- app/src/main/java/com/example/spotify_app/loginScreen/LoginActivity.java
+++ app/src/main/java/com/example/spotify_app/loginScreen/LoginActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.spotify_app.database.StorageSystem;
 
 import com.example.spotify_app.R;
 
@@ -16,14 +17,21 @@ public class LoginActivity extends AppCompatActivity {
     EditText password;
     Button loginButton;
 
+    // StorageSystem db = StorageSystem.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        StorageSystem db = new StorageSystem(getApplicationContext());
+
         username = findViewById(R.id.usernameDialog);
         password = findViewById(R.id.passwordDialog);
         loginButton = findViewById(R.id.loginButton);
+
+        // db.writeLocalAccount(1, "test", "pass_two", 1244);
+        // db.deleteLocalAccount(0);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -33,11 +41,17 @@ public class LoginActivity extends AppCompatActivity {
 
                // something like check if user exists in db
                // for now i just pass in the user
-               if (user.equals("user") && pass.equals("pass")) {
+
+               if (db.readLocalAccountByUserPass(user, pass, "id") != null) {
                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                } else {
                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                }
+//               if (user.equals("user") && pass.equals("pass")) {
+//                   Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+//               } else {
+//                   Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+//               }
            }
         });
 
