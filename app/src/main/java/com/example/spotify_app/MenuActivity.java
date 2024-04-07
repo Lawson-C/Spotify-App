80d3b715062a68014ed28795a8cae9e1605e8bd9
sanjanako <skolla6@gatech.edu>
2024-04-06 21:25:49 -0400

    added menuActivity and navgraph

---
+++ app/src/main/java/com/example/spotify_app/MenuActivity.java
@@ -0,0 +1,31 @@
+package com.example.spotify_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spotify_app.loginScreen.LoginActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        loginButton = findViewById(R.id.loginButton);
+        signupButton = findViewById(R.id.signupButton);
+
+        loginButton.setOnClickListener(v -> openLoginActivity());
+    }
+
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
