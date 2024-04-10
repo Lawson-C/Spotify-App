176051e2ecdce0314fede790ee8a94c682fa9736
aadarsh-v <aadarsh.vavilikolanu@gmail.com>
2024-04-10 03:36:02 -0400

    Allow for audio playback functionality in the spotify wrapped displays.

--- app/src/main/java/com/example/ourspotifyapp/wrappedDisplays/TopTracks.java
+++ app/src/main/java/com/example/ourspotifyapp/wrappedDisplays/TopTracks.java
@@ -2,16 +2,21 @@ package com.example.ourspotifyapp.wrappedDisplays;
 
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.navigation.NavController;
@@ -22,28 +27,49 @@ import com.example.ourspotifyapp.databinding.ActivityTopTracksBinding;
 
 
 import com.example.ourspotifyapp.R;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class TopTracks extends AppCompatActivity {
 
     public static final String CLIENT_ID = "631dbb12d2b642bebb1e019886c20525";
     public static final String REDIRECT_URI = "com.example.ourspotifyapp://auth";
+    public static final int AUTH_TOKEN_REQUEST_CODE = 0;
+    public static final int AUTH_CODE_REQUEST_CODE = 1;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    private String mAccessToken, mAccessCode;
+    private static boolean responseReceived = false;
+    private Call mCall;
     private AppBarConfiguration appBarConfiguration;
     private ActivityTopTracksBinding binding;
-
-    static List<String> topGenres = new ArrayList<>();
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_tracks);
-        Map<String, String> trackToId = TopArtists.getTrackToId();
+        Map<String, String> trackToId = StartingWrappedScreen.getTrackToId();
         List<String> artistNameList = new ArrayList<>();
         int count = 1;
         for (Map.Entry<String, String> artist_id : trackToId.entrySet()) {
@@ -53,56 +79,43 @@ public class TopTracks extends AppCompatActivity {
         }
         Button getGenresButton = (Button) findViewById(R.id.get_top_genres);
 
+        count = 1;
+        String trackIdToPlay = "";
+        Log.d("test", trackToId.toString());
+        for (Map.Entry<String, String> x : trackToId.entrySet()) {
+            Log.d("hmmmm", String.valueOf(x));
+            if (count == 2) {
+                trackIdToPlay = x.getValue();
+            }
+            count++;
+        }
+
+        Log.d("tag yayyy", trackIdToPlay);
+
+        String previewUrl = StartingWrappedScreen.getTrackAudios().get(trackIdToPlay);
+        Log.d("url correct?", previewUrl);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build());
+
+        try {
+            mediaPlayer.setDataSource(previewUrl);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         TextView topTracksTextView = findViewById(R.id.top_tracks_text_view);
-        String formatted = TopArtists.getTopTracksToDisplay().toString().replace("[", "").replace("]", "").replace(",", "\n");
+        String formatted = StartingWrappedScreen.getTopTracksToDisplay().toString().replace("[", "").replace("]", "").replace(",", "\n");
         setTextAsync(formatted, topTracksTextView);
 
         getGenresButton.setOnClickListener((v) -> {
-            calculateTopGenres();
+            mediaPlayer.stop();
             startActivity(new Intent(TopTracks.this, TopGenres.class));
         });
-
-    }
-
-    private void calculateTopGenres() {
-        Map<String, String> myMap = new HashMap<>();
-        myMap = StartingWrappedScreen.getGenreCount();
-        String key1 = "Default, means genres haven't been collected";
-        String key2 = "Default, means genres haven't been collected";
-        String key3 = "Default, means genres haven't been collected";
-        String key4 = "Default, means genres haven't been collected";
-        String key5 = "Default, means genres haven't been collected";
-        int max1 = 0;
-        int max2 = 0;
-        int max3 = 0;
-        int max4 = 0;
-        int max5 = 0;
-        for (Map.Entry<String, String> entry : myMap.entrySet()) {
-            String currKey = entry.getKey();
-            int currVal = Integer.parseInt(entry.getValue());
-            if (currVal > max1) {
-                max1 = currVal;
-                key1 = currKey;
-            } else if (currVal > max2) {
-                max2 = currVal;
-                key2 = currKey;
-            } else if (currVal > max3) {
-                max3 = currVal;
-                key3 = currKey;
-            } else if (currVal > max4) {
-                max4 = currVal;
-                key4 = currKey;
-            } else if (currVal >= max5) {
-                max5 = currVal;
-                key5 = currKey;
-            }
-        }
-
-        topGenres.add(key1);
-        topGenres.add(key2);
-        topGenres.add(key3);
-        topGenres.add(key4);
-        topGenres.add(key5);
 
     }
 
@@ -110,12 +123,6 @@ public class TopTracks extends AppCompatActivity {
         runOnUiThread(() -> textView.setText(text));
     }
 
-    public static List<String> getTopGenres() {
-        return topGenres;
-    }
-    public static void setTopGenres(List<String> topGenres) {
-        TopTracks.topGenres = topGenres;
-    }
 }
 
 
