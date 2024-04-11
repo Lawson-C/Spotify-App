0912114139d0ae691a13559734a3de7f2496825c
Maxim <maxim.bannikov.mb@gmail.com>
2024-04-10 23:06:37 -0700

    adding functionality to settings

--- app/src/main/java/com/example/ourspotifyapp/accountSettingScreen/AccountSettingActivity.java
+++ app/src/main/java/com/example/ourspotifyapp/accountSettingScreen/AccountSettingActivity.java
@@ -1,15 +1,63 @@
 package com.example.ourspotifyapp.accountSettingScreen;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.example.ourspotifyapp.R;
+import com.example.ourspotifyapp.homeScreen.HomeActivity;
+import com.example.ourspotifyapp.loginScreen.LoginActivity;
 
 public class AccountSettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accountsettings);
+        ConstraintLayout chngUser = findViewById(R.id.accountChangeUsernameLayout);
+        ConstraintLayout chngPW = findViewById(R.id.accountChangePasswordLayout);
+        ConstraintLayout deleteAcc = findViewById(R.id.deleteAccount);
+        chngUser.setOnClickListener(v -> changeUser());
+
+    }
+
+    private void changeUser() {
+        Dialog popUp = new Dialog(this);
+        popUp.setContentView(R.layout.changeuserprompt);
+        popUp.show();
+        Button confirm = popUp.findViewById(R.id.confirmdButtonUserChange);
+        Button cancel =  popUp.findViewById(R.id.cancelButtonUserChange);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText oldUser = findViewById(R.id.oldusernameDialog);
+                EditText newUser = findViewById(R.id.newusernameDialog);
+                String oldUserText = oldUser.getText().toString();
+                String newUserText = newUser.getText().toString();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popUp.dismiss();
+            }
+        });
+    }
+
+    private void changePW() {
+    }
+
+    private void deleteAcc() {
     }
 }
+
--- app/src/main/java/com/example/ourspotifyapp/settingScreen/SettingsActivity.java
+++ app/src/main/java/com/example/ourspotifyapp/settingScreen/SettingsActivity.java
@@ -1,15 +1,27 @@
 package com.example.ourspotifyapp.settingScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.ourspotifyapp.R;
+import com.example.ourspotifyapp.accountSettingScreen.AccountSettingActivity;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        ConstraintLayout account = this.findViewById(R.id.accountconstraintLayout);
+        account.findViewById(R.id.accountconstraintLayout);
+        account.setOnClickListener(v -> openAccountSettings());
     }
+
+    private void openAccountSettings() {
+        Intent intent = new Intent(this, AccountSettingActivity.class);
+        startActivity(intent);
+    }
+
 }
